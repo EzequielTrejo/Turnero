762e24e2b855dd8c18f4015bf049b96c3e8caa00
mariano ramos <marianoso1@hotmail.com>
2020-12-07 12:57:21 -0300

    Agregar Clase Cliente con sus propiedades

---
+++ Turnero/Api.Turnero/Data/Entities/Clientes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Turnero.Data.Entities
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string nombre {get; set;}
+
+    }
+}
