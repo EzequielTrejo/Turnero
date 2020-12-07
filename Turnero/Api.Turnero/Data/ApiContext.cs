73773555fb4436e029e1f718288432b43207a4c8
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-12-07 13:26:10 -0300

    Added the db conection string+ context, and installed the dependencies +  the first controller

---
+++ Turnero/Api.Turnero/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Api.Turnero.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Turnero.Data
+{
+    public class ApiContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Turno> Turnos { get; set; }
+    }
+}
