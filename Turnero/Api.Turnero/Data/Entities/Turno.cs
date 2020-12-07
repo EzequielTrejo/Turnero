3af078034cbf0155a09e29be4b4208951f31a297
Ezequiel <50933144+EzequielTrejo@users.noreply.github.com>
2020-12-07 12:39:35 -0300

    Clase Turno

--- Turnero/Api.Turnero/Data/Entities/Turno.cs
+++ Turnero/Api.Turnero/Data/Entities/Turno.cs
@@ -7,5 +7,10 @@ namespace Api.Turnero.Data.Entities
 {
     public class Turno
     {
+        public int Id_Turno { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Cliente { get; set; }
+        public string Servicio { get; set; }
+        public int CostoServicio { get; set; }
     }
 }
