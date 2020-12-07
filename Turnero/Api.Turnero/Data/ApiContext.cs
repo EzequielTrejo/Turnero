968b52cdcaceb4fc03918f7d09ae7b0049ffc846
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-12-07 13:31:39 -0300

    +

--- Turnero/Api.Turnero/Data/ApiContext.cs
+++ Turnero/Api.Turnero/Data/ApiContext.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 
 namespace Api.Turnero.Data
 {
-    public class ApiContext
+    public class ApiContext: DbContext
     {
         public ApiContext(DbContextOptions<ApiContext> options) : base(options)
         {
 
         }
         public DbSet<Turno> Turnos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Servicio> Servicios { get; set; }
     }
 }
